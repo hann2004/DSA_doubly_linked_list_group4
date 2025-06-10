0d7f9bfb15f694d7dd24a473c3579ae88833bec3
Hanan Nasir <148802673+hann2004@users.noreply.github.com>
2025-06-10 21:19:56 +0300

    Update main.cpp
    Handles the includes, color setup, Node struct, and DoublyLinkedList class with constructor/destructor.

--- main.cpp
+++ main.cpp
@@ -1,1 +1,41 @@
+#include <iostream>
+using namespace std;
 
+#define RESET   "\033[0m"
+#define RED     "\033[31m"
+#define GREEN   "\033[32m"
+#define YELLOW  "\033[33m"
+#define CYAN    "\033[36m"
+#define MAGENTA "\033[35m"
+
+struct Node {
+    int data;
+    Node* next;
+    Node* prev;
+
+    Node(int val) : data(val), next(nullptr), prev(nullptr) {}
+};
+
+class DoublyLinkedList {
+private:
+    Node* head;
+    Node* tail;
+    int size;
+
+public:
+    DoublyLinkedList() : head(nullptr), tail(nullptr), size(0) {}
+
+    ~DoublyLinkedList() {
+        clear();
+    }
+
+    void clear() {
+        Node* temp = head;
+        while (temp) {
+            Node* next = temp->next;
+            delete temp;
+            temp = next;
+        }
+        head = tail = nullptr;
+        size = 0;
+    }
