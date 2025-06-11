3360399464b1d3a4a798a1167dce934c5866c2fb
Berutawit-12 <berutawiteyasu@gmail.com>
2025-06-11 09:27:30 +0300

    Update main.cpp
    insertAtBeginning, insertAtEnd, and insertAtPosition with forward display after each insertion.

--- main.cpp
+++ main.cpp
@@ -15,6 +15,59 @@ struct Node {
 
     Node(int val) : data(val), next(nullptr), prev(nullptr) {}
 };
+// Berutawit 
+void insertAtBeginning(int val) {
+        Node* newNode = new Node(val);
+        if (!head) {
+            head = tail = newNode;
+        } else {
+            newNode->next = head;
+            head->prev = newNode;
+            head = newNode;
+        }
+        size++;
+        cout << GREEN << "Inserted " << val << " at the beginning.\n" << RESET;
+        displayForward();
+    }
+
+    void insertAtEnd(int val) {
+        Node* newNode = new Node(val);
+        if (!tail) {
+            head = tail = newNode;
+        } else {
+            tail->next = newNode;
+            newNode->prev = tail;
+            tail = newNode;
+        }
+        size++;
+        cout << GREEN << "Inserted " << val << " at the end.\n" << RESET;
+        displayForward();
+    }
+
+    void insertAtPosition(int val, int pos) {
+        if (pos <= 1) {
+            insertAtBeginning(val);
+            return;
+        }
+
+        Node* temp = head;
+        for (int i = 1; temp && i < pos - 1; ++i) {
+            temp = temp->next;
+        }
+
+        if (!temp || !temp->next) {
+            insertAtEnd(val);
+        } else {
+            Node* newNode = new Node(val);
+            newNode->next = temp->next;
+            newNode->prev = temp;
+            temp->next->prev = newNode;
+            temp->next = newNode;
+            size++;
+            cout << GREEN << "Inserted " << val << " at position " << pos << ".\n" << RESET;
+            displayForward();
+        }
+    }
 
 class DoublyLinkedList {
 private:
